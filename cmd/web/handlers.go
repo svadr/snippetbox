d2e0b3914040996c7fb41f597c1aa4019eb594ea
svadr <salvadorbeltran.dev@gmail.com>
2024-03-07 23:34:01 -0700

    Adding html templates and partials, also configuring home handler to execute template using base that can be shared across multiple pages like home for example in this case

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -14,19 +14,27 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/home.html",
+	}
+
 	// Use the template.ParseFiles() function to read the template file into a
 	// template set. If there's an error, we log the detailed error message and use
 	// the http.Error() function to send a generic 500 Internal Server Error
 	// response to the user. Note that we use the net/http constant
 	// http.StatusInternalServerError here instead of the integer 500 directly.
-	ts, err := template.ParseFiles("./ui/html/home.html")
+	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 
-	err = ts.Execute(w, nil)
+	// Use the ExecuteTemplate() method to write the content of the "base"
+	// template as the response body.
+	err = ts.ExecuteTemplate(w, "base", nil)
 
 	if err != nil {
 		log.Print(err.Error())
