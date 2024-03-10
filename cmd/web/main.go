7579f251584a32a0ea755f9ac84a52a810b34a18
svadr <salvadorbeltran.dev@gmail.com>
2024-03-10 12:17:02 -0700

    Add addr command-line flag for server instance port

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	// Define a new command-line flag with the name 'addr', a default value of ":4000" // and some short help text explaining what the flag controls. The value of the
+	// flag will be stored in the addr variable at runtime.
+	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	// Importantly, we use the flag.Parse() function to parse the command-line flag.
+	// This reads in the command-line flag value and assigns it to the addr
+	// variable. You need to call this *before* you use the addr variable
+	// otherwise it will always contain the default value of ":4000". If any errors are // encountered during parsing the application will be terminated.
+	flag.Parse()
+
 	mux := http.NewServeMux()
 
 	// Create a file server which serves files out of the "./ui/static" directory.
@@ -20,7 +31,15 @@ func main() {
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Print("starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+
+	// The value returned from the flag.String() function is a pointer to the flag
+	// value, not the value itself. So in this code, that means the addr variable
+	// is actually a pointer, and we need to dereference it (i.e. prefix it with
+	// the * symbol) before using it. Note that we're using the log.Printf()
+	// function to interpolate the address with the log message.
+	log.Printf("starting server on %s", *addr)
+
+	// And we pass the dereferenced addr pointer to http.ListenAndServe() too.
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
