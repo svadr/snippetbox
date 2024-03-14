70170beecea54eea4f7dadef13f1dd06c8659126
svadr <salvadorbeltran.dev@gmail.com>
2024-03-13 22:44:17 -0700

    Automatically parse all tempaltes in partials as well for the template cache

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -34,18 +34,22 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		// Extract the file name (like 'home.html') from the full filepath
 		// and assign it to the name variable.
 		name := filepath.Base(page)
-		// Create a slice containing the filepaths for our base template, any
-		// partials and the page.
-		files := []string{
-			"./ui/html/base.html", "./ui/html/partials/nav.html", page,
-		}
-		// Parse the files into a template set.
-		ts, err := template.ParseFiles(files...)
+		// Parse the base template file into a template set.
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
-		// Add the template set to the map, using the name of the page
-		// (like 'home.html') as the key.
+		// Call ParseGlob() *on this template set* to add any partials.
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+		// Call ParseFiles() *on this template set* to add the page template.
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+		// Add the template set to the map as normal...
 		cache[name] = ts
 	}
 	// Return the map.
