a0523824e6c9613088c94e20036d1076de7d3822
svadr <salvadorbeltran.dev@gmail.com>
2024-03-13 19:14:55 -0700

    Use template functions and conditions to show latests snippets

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -24,39 +24,39 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	for _, snippet := range snippets {
-		fmt.Fprintf(w, "%+v\n", snippet)
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/partials/nav.html",
+		"./ui/html/home.html",
 	}
 
-	// files := []string{
-	// 	"./ui/html/base.html",
-	// 	"./ui/html/partials/nav.html",
-	// 	"./ui/html/home.html",
-	// }
+	// Use the template.ParseFiles() function to read the template file into a
+	// template set. If there's an error, we log the detailed error message and use
+	// the http.Error() function to send a generic 500 Internal Server Error
+	// response to the user. Note that we use the net/http constant
+	// http.StatusInternalServerError here instead of the integer 500 directly.
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		// Because the home handler is now a method against the application
+		// struct it can access its fields, including the structured logger. We'll
+		// use this to create a log entry at Error level containing the error
+		// message, also including the request method and URI as attributes to
+		// assist with debugging.
+		app.serverError(w, r, err) // Use the serverError() helper.
+		return
+	}
 
-	// // Use the template.ParseFiles() function to read the template file into a
-	// // template set. If there's an error, we log the detailed error message and use
-	// // the http.Error() function to send a generic 500 Internal Server Error
-	// // response to the user. Note that we use the net/http constant
-	// // http.StatusInternalServerError here instead of the integer 500 directly.
-	// ts, err := template.ParseFiles(files...)
-	// if err != nil {
-	// 	// Because the home handler is now a method against the application
-	// 	// struct it can access its fields, including the structured logger. We'll
-	// 	// use this to create a log entry at Error level containing the error
-	// 	// message, also including the request method and URI as attributes to
-	// 	// assist with debugging.
-	// 	app.serverError(w, r, err) // Use the serverError() helper.
-	// 	return
-	// }
+	// Create an instance of a templateData struct holding the slice of
+	// snippets.
+	data := templateData{
+		Snippets: snippets,
+	}
+	// Pass in the templateData struct when executing the template.
+	err = ts.ExecuteTemplate(w, "base", data)
 
-	// // Use the ExecuteTemplate() method to write the content of the "base"
-	// // template as the response body.
-	// err = ts.ExecuteTemplate(w, "base", nil)
-
-	// if err != nil {
-	// 	app.serverError(w, r, err) // Use the serverError() helper.
-	// }
+	if err != nil {
+		app.serverError(w, r, err) // Use the serverError() helper.
+	}
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
